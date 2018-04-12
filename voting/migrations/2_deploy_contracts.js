2a07237ea5431d4cceed6126d3252848eeb55e11
Unknown <questknight@naver.com>
2018-04-12 14:58:01 +0900

    fix up#2

--- voting/migrations/2_deploy_contracts.js
+++ voting/migrations/2_deploy_contracts.js
@@ -1,8 +1,7 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
-
+var Voting = artifacts.require("./Voting.sol");
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Voting, ['Rama', 'Nick', 'Jose'], {gas: 470000});
 };
+/* As you can see above, the deployer expects the first argument to   be the name of the contract followed by constructor arguments. In our case, there is only one argument which is an array of
+candidates. The third argument is a hash where we specify the gas required to deploy our code. The gas amount varies depending on the size of your contract.
+*/
--- voting/truffle.js
+++ voting/truffle.js
@@ -1,12 +1,11 @@
-// Allows us to use ES6 in our migrations and tests.
 require('babel-register')
-
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1',
-      port: 7545,
-      network_id: '*' // Match any network id
+      host: 'localhost',
+      port: 8092,
+      network_id: '*',
+      gas: 470000
     }
   }
 }
