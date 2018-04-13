5a3fcceb2ad34cf0846187793b9e592f8b2be8f7
KNHui <rlasoaen@naver.com>
2018-04-13 07:58:35 +0000

    add createPoll, insertCandidate, removeCandidate, getVoteCandidateList function

--- voting/contracts/Voting.sol
+++ voting/contracts/Voting.sol
@@ -3,40 +3,77 @@ pragma solidity ^0.4.18;
 
 contract Voting {
     // TODO : getVoteCandidateList
-    struct human{
-        bytes32 name; 
+    struct Poll {
+        bool exists;
+        bytes32 pName;
+        bytes32[] public candidateList;
+
+        // inefficient
+        bytes32[] public cNameList;
+        mapping(bytes32 => Human) public humans;
+    }
+
+    struct Human {
+        bytes32 cName;
         uint8 count;
+    }   
+
+    mapping( uint8 => poll ) public polls;
+    bytes32[] public temp;
+    mapping( bytes32 => Human ) public humans;
+
+    function createPoll( uint8 _roomNumber, bytes32 _pName ) public {
+        polls[_roomNumber] = Poll( true, _pName, , , );
     }
 
-    mapping (bytes32 => human) public votesReceived;
-    
-    bytes32[] public candidateList;
+    function insertCandidate( uint8 _roomNumber, bytes32 _candidate, bytes32 _cName ) public {
+        uint leng = polls[_roomNumber].candidateList.length;
 
-    function getVoteCandidateList(uint8 roomNumber) view public returns (human){
+        // method 1
+        require( !polls[_roomNumber].exists );
         
+        polls[_roomNumber].candidateList[leng] = _candidate;
+        polls[_roomNumber].cNameList[leng] = _cName;
+        polls[_roomNumber].human[_candidate] = Human ( _cName, 0 )
     }
 
-  // This function returns the total votes a candidate has received so far
-    function totalVotesFor(bytes32 votenumber, bytes32 candidate) view public returns (uint8) {
-        require(validCandidate(candidate));
-        return votesReceived[candidate].name;
+    function getVoteCandidateList(uint8 _roomNumber) view public returns ( bytes32[] ) {
+        bytes32[] memory c = polls[_roomNumber].cNameList;
+        return ( c );
     }
 
-  // This function increments the vote count for the specified candidate. This
-  // is equivalent to casting a vote
-
-  //TODO : Edit argc
-    function voteForCandidate(bytes32 candidate) public {
-        require(validCandidate(candidate));
-        votesReceived[candidate] += 1;
+    function removeCandidate( uint8 _roomNumber, bytes32 _candidate ) public {
+        // method 2
+        require( validCandidate( _roomNumber, _candidate ) );
+        delete humans[_roomNumber][_candidate];
     }
 
-    function validCandidate(bytes32 candidate) view public returns (bool) {
-        for(uint i = 0; i < candidateList.length; i++) {
-            if (candidateList[i] == candidate) {
+    // This function returns the total votes a candidate has received so far
+    function totalVotesFor( uint8 _roomNumber, bytes32 _candidate ) view public returns ( uint8 ) {
+        // method 2
+        require( validCandidate( _roomNumber, _candidate ) );
+        return humans[_roomNumber][_candidate].count;
+    }
+
+    // This function increments the vote count for the specified candidate. This
+    // is equivalent to casting a vote
+
+    //TODO : Edit argc
+    function voteForCandidate( uint8 _roomNumber, bytes32 _candidate ) public {
+        // method 2
+        require(validCandidate( _roomNumber, _candidate ) ) ;
+        humans[_roomNumber][_candidate].count += 1;
+    }
+
+    function validCandidate( uint8 _roomNumber, bytes32 _candidate ) view public returns ( bool ) {
+        uint leng = polls[_roomNumber].candidateList.length;
+        for( uint i = 0; i < leng; i++ ) {
+            if ( candidateList[i] == _candidate ) {
                 return true;
             }
         }
         return false;
     }
+
+
 }
