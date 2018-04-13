f5471717dd8e9c5c8b622f9ae2900959b11c682a
KNHui <rlasoaen@naver.com>
2018-04-13 10:37:43 +0000

    update contracts

--- voting/contracts/Voting.sol
+++ voting/contracts/Voting.sol
@@ -4,70 +4,73 @@ pragma solidity ^0.4.18;
 contract Voting {
     // TODO : getVoteCandidateList
     struct Poll {
+        // method 1
         bool exists;
-        bytes32 pName;
         bytes32[] public candidateList;
-
-        // inefficient
-        bytes32[] public cNameList;
+        bytes32[] public NameList;
         mapping(bytes32 => Human) public humans;
     }
 
     struct Human {
-        bytes32 cName;
         uint8 count;
     }
 
     mapping( uint8 => poll ) public polls;
-    bytes32[] public temp;
-    mapping( bytes32 => Human ) public humans;
 
-    function createPoll( uint8 _roomNumber, bytes32 _pName ) public {
-        // possible?
-        polls[_roomNumber] = Poll( true, _pName, , , );
-    }
-
-    function insertCandidate( uint8 _roomNumber, bytes32 _candidate, bytes32 _cName ) public {
-        uint leng = polls[_roomNumber].candidateList.length;
-
-        // method 1
+    function createPoll( uint8 _roomNumber ) public {
         require( !polls[_roomNumber].exists );
-        
-        polls[_roomNumber].candidateList[leng] = _candidate;
-        polls[_roomNumber].cNameList[leng] = _cName;
-        polls[_roomNumber].human[_candidate] = Human ( _cName, 0 )
+
+        polls[_roomNumber] = Poll( true, , , );
     }
 
-    function getVoteCandidateList(uint8 _roomNumber) view public returns ( bytes32[] ) {
-        bytes32[] memory c = polls[_roomNumber].cNameList;
-        return ( c );
+    function removePoll( uint8 _roomNumber ) public {
+        require( polls[_roomNumber].exists );
+
+        delete polls[_roomNumber];
+    }
+
+
+    function insertCandidate( uint8 _roomNumber, bytes32 _candidate, bytes32 _Name ) public {
+        require( !validCandidate( _roomNumber, _candidate ) );  
+   
+        polls[_roomNumber].candidateList.push(_candidate);
+        polls[_roomNumber].NameList.push(_Name);
+        polls[_roomNumber].human[_candidate] = Human ( 0 );
     }
 
     function removeCandidate( uint8 _roomNumber, bytes32 _candidate ) public {
-        // method 2
         require( validCandidate( _roomNumber, _candidate ) );
-        delete humans[_roomNumber][_candidate];
+
+        delete polls[_roomNumber][_candidate];
+    }
+
+    function getCandidateList(uint8 _roomNumber) view public returns ( bytes32[] ) {
+        bytes32[] memory c;
+        require( polls[_roomNumber].exists );
+        c = polls[_roomNumber].NameList;
+        return ( c );
     }
 
     // This function returns the total votes a candidate has received so far
     function totalVotesFor( uint8 _roomNumber, bytes32 _candidate ) view public returns ( uint8 ) {
-        // method 2
         require( validCandidate( _roomNumber, _candidate ) );
-        return humans[_roomNumber][_candidate].count;
+        return polls[_roomNumber][_candidate].count;
     }
 
     // This function increments the vote count for the specified candidate. This
     // is equivalent to casting a vote
-
     //TODO : Edit argc
     function voteForCandidate( uint8 _roomNumber, bytes32 _candidate ) public {
-        // method 2
         require(validCandidate( _roomNumber, _candidate ) ) ;
-        humans[_roomNumber][_candidate].count += 1;
+        polls[_roomNumber][_candidate].count += 1;
     }
 
+    // method 2
     function validCandidate( uint8 _roomNumber, bytes32 _candidate ) view public returns ( bool ) {
-        uint leng = polls[_roomNumber].candidateList.length;
+        uint leng;
+        require( polls[_roomNumber].exists );
+        leng = polls[_roomNumber].candidateList.length;
+
         for( uint i = 0; i < leng; i++ ) {
             if ( polls[_roomNumber].candidateList[i] == _candidate ) {
                 return true;
